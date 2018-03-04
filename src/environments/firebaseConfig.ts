547f050cf3c2ded9776bf0b4ebc1d68e153cfc4c
Dimitar Mitev <dnmitev@gmail.com>
2018-03-04 11:57:18 +0200

    Initial commit by ngx-firebase-bootstrap generator

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,11 @@
+
+
+export const firebaseConfig = {
+    // Paste all this from the Firebase console...
+    apiKey: "AIzaSyByBlvbtY8GkFj8zMCyo98MasP1MuNDWK8",
+    authDomain: "bulprosblog.firebaseapp.com",
+    databaseURL: "https://bulprosblog.firebaseio.com",
+    storageBucket: "bulprosblog.appspot.com",
+    messagingSenderId: "1003093706142"
+};
+
